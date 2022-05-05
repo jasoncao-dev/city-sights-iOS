4ff08339dbc4132a02b520795e640071c06ead11
Trung Cao <caot1@nku.edu>
2022-05-04 22:01:49 -0400

    Initialize set up for project

---
+++ CitySightsApp/CitySightsApp/CitySightsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsAppApp.swift
+//  CitySightsApp
+//
+//  Created by Jason Cao on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LaunchView()
+        }
+    }
+}
---
+++ CitySightsApp/CitySightsApp/Views/LaunchView.swift
@@ -0,0 +1,21 @@
+//
+//  LaunchView.swift
+//  CitySightsApp
+//
+//  Created by Jason Cao on 5/4/22.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct LaunchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchView()
+    }
+}
